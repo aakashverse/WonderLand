23828563126b9d599c48c2ab3e3034df133bcff7
planet-akash <akashyadavakash03@gmail.com>
2025-08-07 17:41:02 +0530

    corrected server error

--- app.js
+++ app.js
@@ -20,13 +20,11 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 // Database connection
-// const Mongourl = 'mongodb://127.0.0.1:27017/wonderland';
 const dbUrl = process.env.ATLAS_URL;
 mongoose.connect(dbUrl)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-// Express setup
 const app = express();
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -37,16 +35,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Session config
 const store = MongoStore.create({
   mongoUrl: dbUrl,
-  crypto:{
+  crypto: {
     secret: process.env.SECRET,
   },
   touchAfter: 24*3600,
 });
 
-store.on("error",()=>{
-  console.log("ERROR in MONGO SESSION STORE",err);
+store.on("error", (err) => {
+  console.log("ERROR in MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
@@ -55,56 +54,57 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: true,
   cookie: {
-      expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true,
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
   },
 };
+
 app.use(session(sessionOptions));
 app.use(flash());
 
+// Passport initi
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser());      // to gather user info in particular session
-passport.deserializeUser(User.deserializeUser());  // to remove user info in particular session
 
-app.use((req,res,next)=>{
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.curUser = req.user;
+  res.locals.curUser = req.user; 
   next();
 });
 
-// Basic test route - MUST WORK FIRST
-// app.get('/', (req, res) => {
-//   res.send('Hi, I am root!');
-// });
+// Routes
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
-// 404 handler
-app.all("*",(req,res,next)=>{
-    next(new ExpressError(404,"Page Not Found"));
-});
-
-app.get("/demouser",async(req,res)=>{
+// Test route
+app.get("/demouser", async (req, res) => {
   let fakeUser = new User({
     email: "akash@gmail.com",
     username: "aakashTheOG",
   });
   
-  let registeredUser = await User.register(fakeUser,"iamakashOG");
+  let registeredUser = await User.register(fakeUser, "iamakashOG");
   res.send(registeredUser);
 });
 
-app.use("/listings", listingRouter);
-app.use("/listings/:id/reviews", reviewRouter);
-app.use("/", userRouter);
+// 404 handler
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
+});
 
 // Error handler
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = 'Something went wrong!' } = err;
-  res.status(statusCode).render("error.ejs", {err});
+  res.status(statusCode).render("error.ejs", { err });
 });
 
 const port = 8080;
